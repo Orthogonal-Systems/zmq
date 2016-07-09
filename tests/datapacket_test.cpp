78398e14285092dd0d97fc718325330723147c50
Matthew Ebert <mfe5003@gmail.com>
2016-07-09 09:26:00 -0500

    preparing to split the repository

--- tests/datapacket_test.cpp
+++ tests/datapacket_test.cpp
@@ -26,11 +26,17 @@ int main(){
   int16_t data[channels] = {0xef34,0x2312};
   char regResp[9] = "[0,\xAA\xBB\xCC\xDD]"; // generate success response with fake streamKey
   memcpy(buffer, regResp, 8);
-  packet.processStreamRegistration(8);      // process the fake response
+  uint8_t err = packet.processStreamRegistration(8);      // process the fake response
+  cout << "Registration processing error code: " << int(err) << endl;
   packet.preparePacket(time, frac_sec, data); // send out a data packet
   cout << "data in buffer (bytes): " << int(packet.getPacketSize()) << endl;
   for(uint8_t j=0; j<packet.getPacketSize(); j++){
     printf("%02X:", buffer[j]&0x000000FF);
   }
   cout << endl;
+
+  // try a error in the response
+  memcpy(buffer, "[1,\xAA\xBB\xCC\xDD]", 8);
+  err = packet.processStreamRegistration(9);      // process the fake response
+  cout << "Registration processing error code: " << int(err) << endl;
 }
